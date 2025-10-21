8050af5187e66be605caba7d05f2b58714597be0
JSeHoone <beebea545@gmail.com>
2025-10-21 22:06:51 +0900

    test : test 예제 추가

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,13 +1,12 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
-    public class AppTest2 {
-        @Test
-        void testApp() {
-            assert
-        }
+    @Test
+    void testApp() {
+        assertEquals(2, 1+1);
     }
 }
