ba5401941084d234909334b801264adc53756ec5
JSeHoone <beebea545@gmail.com>
2025-10-21 22:06:13 +0900

    feat : ci 테스트 파일 추가

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    public class AppTest2 {
+        @Test
+        void testApp() {
+            assert
+        }
+    }
+}
